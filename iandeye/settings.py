621525def79e1b9cb67a53f8e2a046dc7ab06928
vinodgajula <vinodgajula2612@gmail.com>
2022-03-14 09:37:16 +0530

    configurations for heroku

--- iandeye/settings.py
+++ iandeye/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import django_heroku
+django_heroku.settings(locals())
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
